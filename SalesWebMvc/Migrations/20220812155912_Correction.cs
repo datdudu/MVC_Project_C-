8e987662faab0feee23edbeb9657b43edefcc85d
datdudu <c.eduardocarvalhoc@gmail.com>
2022-08-12 13:39:43 -0300

    Simple Create Form

---
+++ SalesWebMvc/Migrations/20220812155912_Correction.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SalesWebMvc.Migrations
+{
+    public partial class Correction : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "DateTime",
+                table: "Seller",
+                newName: "BirthDate");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "BirthDate",
+                table: "Seller",
+                newName: "DateTime");
+        }
+    }
+}
